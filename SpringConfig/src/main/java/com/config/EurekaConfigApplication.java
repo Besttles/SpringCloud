380fa0c36c375fcd313278dfa994c934b0092a46
Besttles <bwh15543272388@163.com>
2019-10-31 23:55:07 +0800

    upload the file
    upload the file

--- SpringConfig/src/main/java/com/config/EurekaConfigApplication.java
+++ SpringConfig/src/main/java/com/config/EurekaConfigApplication.java
@@ -2,13 +2,17 @@ package com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by biwh on 2019/10/26.
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class EurekaConfigApplication {
 
     public static void main(String[] args) {
